c4343bd68f3336c477e216a7f70adcf09af7ed87
npicciano79 <npicciano79@gmail.com>
2022-02-27 14:40:32 -0500

    repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
+const repeatString = function(str,times) {
+    
+    let fullString=''
+    rs:if (str==""){
+        break rs;
+    }else if (times<0){
+        fullString="ERROR";
+        break rs;
+    }else{
+        for(let i=0;i<times;i++){
+            fullString+=str;
+        }
+             
+    }return fullString;
+}
+    
 
-};
+let repeatedString='hey';
+let numberRepeat=3;
+
+
+repeatString(repeatedString,numberRepeat);
 
 // Do not edit below this line
 module.exports = repeatString;
