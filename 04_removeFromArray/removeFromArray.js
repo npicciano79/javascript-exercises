52adf442535b1aa55a4bafaae5839ea0d1d36ed7
npicciano79 <npicciano79@gmail.com>
2022-03-01 05:40:15 -0500

    removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...val) {
 
-};
+    let newArr=arr.filter(a => !val.includes(a));
+    return newArr;
+
+    }
+
+
+
+let arr=['hey', 2, 3, 'ho'];
+//let val=[3,2];
+removeFromArray(arr,'1',3);
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
