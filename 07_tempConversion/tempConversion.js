cd6d55a1f4a4414e067ae51702d13a59d7f319fd
npicciano79 <npicciano79@gmail.com>
2022-03-02 18:44:30 -0500

    tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,22 @@
-const ftoc = function() {
+const ftoc = function(faren) {
+  var cel=(((faren-32)/9)*5);
+  cel= Number(cel.toFixed(1))
+
+  return cel;
 
 };
 
-const ctof = function() {
+const ctof = function(cel) {
+  var faren=((9*cel)/5)+32;
+  
+  return Number(faren.toFixed(1));
+
 
 };
+
+ctof(-10);
+
+
 
 // Do not edit below this line
 module.exports = {
