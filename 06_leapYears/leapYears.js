3ea6c2fc10e5ca73f7313eef669096549e9db115
npicciano79 <npicciano79@gmail.com>
2022-03-02 17:02:00 -0500

    leapYears/js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //divisible by 4 and not 100
+    if (year%4==0 && year%100!=0){     
+        return true;
+    }else if (year%400==0){
+        return true
+    }else{
+        return false
+    }
 
 };
+
+
+leapYears(700);
 
 // Do not edit below this line
 module.exports = leapYears;
