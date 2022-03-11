487351e92f785999687dd32448f0355b5a1d5d2d
npicciano79 <npicciano79@gmail.com>
2022-03-11 15:59:29 -0500

    fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let prevVal=0;
+    let currVal=1;
+    let count=1;
+    if (number<0){
+        return 'OOPS'
+    }
+    while(count<number){
+        let tempVal=currVal;
+        currVal=currVal+prevVal;
+        prevVal=tempVal;
+        count+=1
+    }
+    return currVal;
 
 };
+
+fibonacci(8);
 
 // Do not edit below this line
 module.exports = fibonacci;
