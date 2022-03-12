3c8eaec7cdcfac63f1ef2e15197741c327a85037
npicciano79 <npicciano79@gmail.com>
2022-03-12 08:49:58 -0500

    getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,38 @@
-const getTheTitles = function() {
+const getTheTitles = function(books,field){ 
+  //loop through array of objects,, returns array of titles + additional array, fix
+  let answer=[]
+  for (let i=0;i<books.length;i++){
+    answer.push(books[i][field]);
+    
+  }
+  return answer;
 
-};
 
+
+
+
+
+  //using map function
+  //let result = books.map(a => a.title);
+  
+  //return result;
+  
+}
+  
+
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+let field='title';
+getTheTitles(books,field);
 // Do not edit below this line
 module.exports = getTheTitles;
