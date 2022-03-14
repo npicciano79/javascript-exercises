8ba19bc451e4e9f52ba08a7dceabb871c9f55830
npicciano79 <npicciano79@gmail.com>
2022-03-14 08:41:05 -0400

    caesar.js

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,46 +1,56 @@
-const caesar = function(str,shift) {
+const caesar = function(str,shift){
+    //computes shift factor if >26
+    if (shift>26){
+        shift=shift%26;
+    } 
+    //itterates through String str 
+     for (let i=0; i<str.length;i++){
+         //converts str character to ASCII value 
+         let strCode=str[i].charCodeAt(str[i]); 
+         //console.log(strCode);
+         //test for capital letter
+         if (strCode>=65 && strCode<=90){
+             //capital letter, test if shift returns special char
+             //edits shift to return capital letter
+             if ((strCode+shift)>90){
+                 strCode=((strCode+shift)-90)+64;
+             //negative shift
+             }else if ((strCode+shift)<65){
+                 strCode=(91+(shift+strCode-65))
+             }
+             //shift between 65 and 90
+             else{
+                 strCode+=shift;
+         }
+         //test for lowercase letters, shift if strCode outside of 122
+         }else if(strCode>=97 && strCode<=122){
+             //shift greater than 122, return to beginning of alpha 
+             if ((strCode+shift)>122){
+                 strCode=(strCode+shift-122)+96;
+             //negative shift
+             }else if ((strCode+shift)<97){
+                 strCode=(123+(shift+strCode-97));
+             }
+             //shift between 97 and 122
+             else{
+                 strCode+=shift;
+             }
+         }
+         //no shift for special characters
+         //convert strCode back to character
+         shiftChar=String.fromCharCode(strCode);
+         shiftWord+=shiftChar;
+         } 
 
-   //itterates through String str 
-    for (let i=0; i<str.length/4;i++){
-        //converts str character to ASCII value 
-        let strCode=str[i].charCodeAt(str[i]); 
-        //test for capital letter
-        if (strCode>=65 && strCode<=90){
-            //capital letter, test if shift returns special char
-            //edits shift to return capital letter
-            if ((strCode+shift)>90){
-                strCode=((strCode+shift)-90)+64;
-            }else{
-                strCode+=shift;
-            }
-          
-        //test for lowercase letters, shift if strCode outside of 122
-        }else if(strCode>=97 && strCode<=122){
-            if ((strCode+shift)>122){
-                strCode=(strCode-122)+96;
-            }else{
-                //within 97-122, does not loop through characters
-                strCode+=shift          
-            }
-        }
-        //else special character, returns same character, no shift
-        //convert strCode back to character
-
-        shiftChar=String.fromCharCode(strCode);
-        
-        //adds shifted char to returned word
-    shiftWord+=shiftChar;
-    return shiftWord;
-
-    }       
-    //return shiftChar; 
-}
+    return shiftWord;  
+    }
+    
 
 
-//let shiftChar='';
+
 let shiftWord='';
-let shift=5;
-let str='Hello, World!';
+let shift=1;
+let str='Aaa';
 caesar(str,shift);
 // Do not edit below this line
 module.exports = caesar;
